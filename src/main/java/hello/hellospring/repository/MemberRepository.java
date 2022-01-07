5e5150d61f4fa8a9a1b7a717de1bb2cb0374e2fe
lokihyeon <hae8573@koreatech.ac.kr>
2022-01-07 23:19:06 +0900

    Jpa

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 public interface MemberRepository {
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -18,15 +18,17 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class MemberServiceIntegrationTest {
 
-    @Autowired MemberService memberService;
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
 
 
     @Test
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("hello100");
 
         //when
         Long saveId = memberService.join(member);
@@ -39,37 +41,18 @@ class MemberServiceIntegrationTest {
 
 
     @Test
-    public void 중복_회원_예외(){
-        //given
+    public void 중복_회원_예외() {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
-
-
-        //when
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
 
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e3){
-            assertThat(e3.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
-
-
-        //then
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
     }
 }
